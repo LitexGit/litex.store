0ba1f31fe1c9ba1785c9618b3cdd7e30f386ddda
zhoujian <jian.zhou@litex.io>
2019-06-25 16:57:38 +0800

    feat(pages/Gas.vue): add pages/Gas.vue
    add pages/Gas.vue

---
+++ src/pages/Gas.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page padding class="flex">
+    <q-card class="q-pa-sm container">
+      <q-card-section class="">
+
+        <q-btn-dropdown split icon='close' color="primary" label="1000110123456789012">
+            <q-list link>
+              <q-item v-for="card in cards" :key="card.gameUserid" close-overlay>
+               <span>{{card}}</span>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+
+      </q-card-section>
+
+      <q-card-section class="bg-red">
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Gas',
+  data () {
+    return {
+      cards: [
+        {
+          chargeType: '1',
+          gameUserid: '1000110123456789012',
+          gasCardTel: '18516804325',
+          gasCardName: 'name001'
+        },
+        {
+          chargeType: '2',
+          gameUserid: '9010001101234567',
+          gasCardTel: '18516804325',
+          gasCardName: 'name002'
+        }
+      ]
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  flex: 1
+}
+</style>
