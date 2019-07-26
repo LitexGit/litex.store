f54f3872241013c300d2de58ff580d5d640e3e9c
zhoujian <jian.zhou@litex.io>
2019-07-26 16:21:41 +0800

    feat(ChannelStatusBar.vue): update ChannelStatusBar
    update ChannelStatusBar

--- src/components/ChannelStatusBar.vue
+++ src/components/ChannelStatusBar.vue
@@ -1,12 +1,32 @@
+<!--
 <template>
-  <div v-if="status" :class="bg" class="flex justify-end text-white">
+  <div v-if="isShow" :class="bg" class="flex justify-end text-white">
     <small class="q-pa-xs q-pr-md">{{ message }}</small>
   </div>
 </template>
 
+<template>
+  <div v-if="isShow" :class="bg" class="row justify-end items-center" style="background-color: orange">
+    <small class="q-pr-sm text-weight-medium">{{ message }}</small>
+    <div class="self-end q-pr-md">
+      <vue-loading type="cylon" color="white" :size="{ width: '30px', height: '30px' }"/>
+    </div>
+  </div>
+</template>
+-->
+
+<template>
+  <div v-if="isShow" :class="bg" class="row justify-center items-center">
+    <vue-loading type="cylon" color="white" class="bg-orange" :size="{ width: '30px', height: '30px' }"/>
+  </div>
+</template>
+
 <script>
+import { VueLoading } from 'vue-loading-template'
+
 export default {
   name: 'ChannelStatusBar',
+  components: { VueLoading },
   props: {
     status: {
       required: true
@@ -18,6 +38,9 @@ export default {
     },
     bg: function () {
       return this.status === 2 ? 'bg-orange' : ''
+    },
+    isShow: function () {
+      return this.status === 2
     }
   }
 }
--- src/store/pn/actions.js
+++ src/store/pn/actions.js
@@ -5,13 +5,13 @@ export async function updatePrice ({ commit, rootGetters, rootState }, payload) 
   commit('loading', true)
   const { selectGoods: { price } } = rootState.phone
   const { tokens, selected } = rootState.config
-  const { address, float } = tokens[selected]
+  const { address, round } = tokens[selected]
   const { tokens: rates } = await api.getRates([{ address }])
   const Ramda = require('ramda')
   const { rate } = Ramda.head(rates)
 
   let decimal = price / 100 / rate
-  decimal = mathCeil({ decimal, float })
+  decimal = mathCeil({ decimal, round })
   commit('updatePrice', decimal)
   commit('loading', false)
 }
